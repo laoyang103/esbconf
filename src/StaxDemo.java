3523434c9535335a42fb2a94988e313a7abb0909
laoyang103 <553069938@qq.com>
2018-12-13 14:22:26 +0800

    单交易导入

--- src/StaxDemo.java
+++ src/StaxDemo.java
@@ -161,7 +161,12 @@ public class StaxDemo {
       String transName = ((String )svc.get("SvcDesc"));
       if (transName.length() > 20) transName = transName.substring(0, 20);
 
+      // xml
       // if (!"乐益通".equals(transName)) continue;
+      // common
+      // if (!"个人客户信息建立".equals(transName)) continue;
+      // 8583
+      // if (!"本行卡预授权打包".equals(transName)) continue;
       System.out.printf("Try Add trans: [transName=%s] [transCode=%s] \n", transName, transCode);
 
       JSONArray transList = UrlImport.addMockTrans(systemCode, systemType, transCode, transName, messageType, messageEncoding);
